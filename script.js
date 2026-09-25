c7aa631edd6ede70b4049783e5b4f1f2571301cd
russvitalik0-lgtm <russvitalik0@gmail.com>
2026-09-25 18:01:38 +0300

    добавление иконок, перепланирование структуры и переменные в css

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const w = 50
 const h = 30
+let grid = document.querySelector('.grid')
 function makeGrid(w, h) {
-    for (let i = 0; i<w; i+=1) {
-        
+    grid.style.gridTemplateRows = `repeat(${h}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${w}, 0.6fr)`
+    for (let i = 0; i<h; i+=1) {
+        for (let j = 0; j<w; j+=1) {
+            let pixel = document.createElement('div')
+            pixel.classList.add('pixel')
+            pixel.id = i+'-'+j
+            grid.appendChild(pixel)
+        }
     }
 }
+makeGrid(w, h)
